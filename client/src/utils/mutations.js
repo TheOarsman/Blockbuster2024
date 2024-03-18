e4537d82d4cef2b70e5f8a1acbcefefdc9127f3a
Goobergreve09 <gregory.greve@yahoo.com>
2024-03-18 18:24:52 -0400

    updated mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,15 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
 
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
@@ -11,4 +20,5 @@ export const ADD_USER = gql`
         username
       }
     }
-  }`
+  }
+`;
