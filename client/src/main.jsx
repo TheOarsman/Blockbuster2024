a48d65d9542442f2c5b6be7387c5ed526e28d2e2
TheOarsman <heinzulrichv@gmail.com>
2024-03-18 22:14:32 -0400

    membership card working (for now)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,45 +1,44 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import App from './App.jsx';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import SearchMovies from './pages/SearchMovies'; 
-import MyFavoritedMovies from './pages/FavoritedMovies';
-import Home from './pages/Home';
+import App from "./App.jsx";
+// import SearchBooks from './pages/SearchBooks';
+// import SavedBooks from './pages/SavedBooks';
+// import SearchMovies from './pages/SearchMovies';
+// import MyFavoritedMovies from './pages/FavoritedMovies';
+// import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
     children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: '/search-books',
-        element: <SearchBooks />
-      }, 
-      {
-        path: '/saved-books',
-        element: <SavedBooks />
-      },
-      {
-        path: '/search-movies',
-        element: <SearchMovies />
-      },
-      {
-        path: '/favorite-movies',
-        element: <MyFavoritedMovies />
-      },
-
+      // {
+      //   index: true,
+      //   element: <Home />
+      // },
+      // {
+      //   path: '/search-books',
+      //   element: <SearchBooks />
+      // },
+      // {
+      //   path: '/saved-books',
+      //   element: <SavedBooks />
+      // },
+      // {
+      //   path: '/search-movies',
+      //   element: <SearchMovies />
+      // },
+      // {
+      //   path: '/favorite-movies',
+      //   element: <MyFavoritedMovies />
+      // },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
