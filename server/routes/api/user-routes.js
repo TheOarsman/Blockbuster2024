5d0f9206f14ba3729b550c011e343740bdc5138a
Goobergreve09 <gregory.greve@yahoo.com>
2024-03-18 20:14:55 -0400

    edited user-routes per Vince Shury

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -3,19 +3,25 @@ const {
   createUser,
   getSingleUser,
   saveBook,
+  saveMovie,
   deleteBook,
+  deleteMovie,
   login,
-} = require('../../controllers/user-controller');
+} = require('../../controllers/user-controllers');
 
 // import middleware
 const { authMiddleware } = require('../../utils/auth');
 
 // put authMiddleware anywhere we need to send a token for verification of user
-router.route('/').post(createUser).put(authMiddleware, saveBook);
+router.route('/').post(createUser);
 
 router.route('/login').post(login);
 
 router.route('/me').get(authMiddleware, getSingleUser);
+
+router.route('/books').post(authMiddleware, saveBook);
+
+router.route('/movies').post(authMiddleware, saveMovie);
 
 router.route('/books/:bookId').delete(authMiddleware, deleteBook);
 
