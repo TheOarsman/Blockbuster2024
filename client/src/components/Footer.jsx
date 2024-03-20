9f95fb4a7bf0aa5e70b797f074c254c8dbe8f527
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-03-20 14:23:05 -0400

    Added footer component file

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+
+  return (
+    <>
+      <div>
+        <footer>
+
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
