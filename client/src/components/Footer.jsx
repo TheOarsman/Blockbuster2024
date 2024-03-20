1bb9bbcdbaf55a1ad3f2108126eb11bc2bdeafe3
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-03-20 15:13:37 -0400

    Added HTML for lists of developer profiles

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,40 +7,45 @@ const Footer = () => {
     <>
       <div>
         <footer>
-            {/* Sections for our developer info */}
+            {/* Sections for our developer info (GitHub, LinkedIn, some third thing) */}
             <div>
                 Ethan Wynne
                 <ul>
                     <li><a href="https://github.com/ethanfrog">GitHub</a></li>
                     <li><a href="https://www.linkedin.com/in/ethan-wynne-b2a956161/">LinkedIn</a></li>
+                    <li></li>
                 </ul>
             </div>
             <div>
                 Whitney Simpson
                 <ul>
-                    <li>GitHub</li>
-                    <li>LinkedIn</li>
+                    <li><a href="">GitHub</a></li>
+                    <li><a href="">LinkedIn</a></li>
+                    <li></li>
                 </ul>
             </div>
             <div>
                 Heinz Ulrich
                 <ul>
-                    <li>GitHub</li>
-                    <li>LinkedIn</li>
+                    <li><a href="">GitHub</a></li>
+                    <li><a href="">LinkedIn</a></li>
+                    <li></li>
                 </ul>
             </div>
             <div>
                 Greg Greve
                 <ul>
-                    <li>GitHub</li>
-                    <li>LinkedIn</li>
+                    <li><a href="">GitHub</a></li>
+                    <li><a href="">LinkedIn</a></li>
+                    <li></li>
                 </ul>
             </div>
             <div>
                 Nicholas Eggleston
                 <ul>
-                    <li>GitHub</li>
-                    <li>LinkedIn</li>
+                    <li><a href="">GitHub</a></li>
+                    <li><a href="">LinkedIn</a></li>
+                    <li></li>
                 </ul>
             </div>
         </footer>
