d080018ecff9d6bb7791485f66b831ee70da43c3
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-03-20 15:09:46 -0400

    Added HTML for developer links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,42 @@ const Footer = () => {
     <>
       <div>
         <footer>
-
+            {/* Sections for our developer info */}
+            <div>
+                Ethan Wynne
+                <ul>
+                    <li><a href="https://github.com/ethanfrog">GitHub</a></li>
+                    <li><a href="https://www.linkedin.com/in/ethan-wynne-b2a956161/">LinkedIn</a></li>
+                </ul>
+            </div>
+            <div>
+                Whitney Simpson
+                <ul>
+                    <li>GitHub</li>
+                    <li>LinkedIn</li>
+                </ul>
+            </div>
+            <div>
+                Heinz Ulrich
+                <ul>
+                    <li>GitHub</li>
+                    <li>LinkedIn</li>
+                </ul>
+            </div>
+            <div>
+                Greg Greve
+                <ul>
+                    <li>GitHub</li>
+                    <li>LinkedIn</li>
+                </ul>
+            </div>
+            <div>
+                Nicholas Eggleston
+                <ul>
+                    <li>GitHub</li>
+                    <li>LinkedIn</li>
+                </ul>
+            </div>
         </footer>
       </div>
     </>
