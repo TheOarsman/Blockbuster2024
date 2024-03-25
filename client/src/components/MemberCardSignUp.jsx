c746aaa79e708395904b96487a2ae73c14d38b48
TheOarsman <heinzulrichv@gmail.com>
2024-03-25 13:34:49 -0400

    MemeberCard Styling

--- client/src/components/MemberCardSignUp.jsx
+++ client/src/components/MemberCardSignUp.jsx
@@ -5,35 +5,42 @@ import MembershipCardImg from "../assets/images/BlockbusterMembership03.png"; //
 function MemberCardSignUp({ username, email, paddedUsername, memberSince }) {
   return (
     <div className="d-flex flex-column">
-    <Card style={{ width: "100%", border: "none", margin: "auto" }}>
-      <Card.Img src={MembershipCardImg} alt="Membership Card" />
-      <Card.ImgOverlay className="d-flex flex-column justify-content-center align-items-center cardText">
-   
-      <div className = 'username-container'>
-        <Card.Title className = 'username'>Username: {username}</Card.Title>
-        </div>
-        <div className = 'email-container'>
-        <Card.Title className = 'email'>Email: {email}</Card.Title>
-        </div>
- 
-        <div className="barcode-container">
-          <div className = 'barcode'>
-          <ReactBarcode
-            value={paddedUsername.padEnd(20, ".")}
-            width={1.0} // Set the width of the bars
-            height={14} // Set the height of the bars
-            format="CODE128" // Specify the barcode format
-            displayValue={false} // Hide the human-readable text
-            textPosition="none" // Hide the text completely
-            background="#2556A5" // Set the background color
-          />
+      <Card
+        style={{
+          width: "100%",
+          border: "none",
+          margin: "auto",
+          background: "transparent",
+        }}
+      >
+        <Card.Img src={MembershipCardImg} alt="Membership Card" />
+        <Card.ImgOverlay className="d-flex flex-column justify-content-center align-items-center cardText">
+          <div className="username-container">
+            <Card.Title className="username">Username: {username}</Card.Title>
+          </div>
+          <div className="email-container">
+            <Card.Title className="email">Email: {email}</Card.Title>
+          </div>
+          <div className="memberSince-container">
+            <Card.Text className="memberSince">
+              Member Since: {memberSince}
+            </Card.Text>
+          </div>
+          <div className="barcode-container">
+            <div className="barcode">
+              <ReactBarcode
+                value={paddedUsername.padEnd(20, ".")}
+                width={1.0} // Set the width of the bars
+                height={14} // Set the height of the bars
+                format="CODE128" // Specify the barcode format
+                displayValue={false} // Hide the human-readable text
+                textPosition="none" // Hide the text completely
+                background="transparent" // Set the background color
+              />
             </div>
           </div>
-          <div className = 'memberSince-container'>
-        <Card.Text className = 'memberSince'>Member Since: {memberSince}</Card.Text>
-        </div>
-      </Card.ImgOverlay>
-    </Card>
+        </Card.ImgOverlay>
+      </Card>
     </div>
   );
 }
